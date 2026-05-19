7f0708652275835fa74ab2cef044104d62e127c3
Rohan Vishwakarma <vishwakarma989345@gmail.com>
2026-05-20 00:19:39 +0530

    Update Tringale Pattern Program.cpp

--- Tringale Pattern Program.cpp
+++ Tringale Pattern Program.cpp
@@ -5,10 +5,10 @@ int main()
 int n;
 cout<<"entar number";
 cin>>n;
-for(int i=0;i<=n;i++){
+for(int i=1;i<=n;i++){
 	
 	
-for(int j=0;j<=i;j++){ 
+for(int j=1;j<=i;j++){ 
 	if((i+j) % 2==0)
 	cout<<1<<" ";
 	else
